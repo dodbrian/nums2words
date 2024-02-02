f149910ada5710a41c0c96dceb3bdf71416c8392
Denis Zimin <roydronov@gmail.com>
2024-02-02 16:23:09 +0100

    refactor converter class

--- Nums2Words.Domain/NumberToWordsConverter.cs
+++ Nums2Words.Domain/NumberToWordsConverter.cs
@@ -2,7 +2,7 @@
 
 namespace Nums2Words.Domain;
 
-public static class NumberToWordsConverter
+public class NumberToWordsConverter
 {
     private static readonly Dictionary<int, string> Map = new()
     {
@@ -37,34 +37,45 @@ public static class NumberToWordsConverter
     };
 
     private static readonly string[] Magnitude = { "thousand", "million" };
-
     private const string Hundred = "hundred";
 
-    public static string Convert(decimal number)
-    {
-        var integral = (int)number;
-        var fractional = (int)((number - integral) * 100);
+    private StringBuilder? _builder;
 
-        var builder = new StringBuilder();
+    public string Convert(decimal number)
+    {
+        var absoluteNumber = Math.Abs(number);
+
+        if (absoluteNumber > 999_999_999.99m)
+            throw new InvalidOperationException("The absolute value should not be higher than 999 999 999,99");
+
+        var integral = (int)absoluteNumber;
+        var fractional = (int)((absoluteNumber - integral) * 100);
+
+        _builder = new StringBuilder();
+
         if (fractional != 0)
         {
-            builder.Append(fractional == 1 ? " cent" : " cents");
-            ConvertInternal(fractional, builder);
-            builder.Insert(0, " and");
+            _builder.Append(fractional == 1 ? " cent" : " cents");
+            ConvertInternal(fractional);
+            _builder.Insert(0, " and");
         }
 
-        builder.Insert(0, integral == 1 ? " dollar" : " dollars");
-        ConvertInternal(integral, builder);
+        _builder.Insert(0, integral == 1 ? " dollar" : " dollars");
+        ConvertInternal(integral);
 
-        return builder.ToString().Trim();
+        if (number < 0) _builder.Insert(0, "minus");
+
+        return _builder.ToString().Trim();
     }
 
-    private static void ConvertInternal(int number, StringBuilder builder)
+    private void ConvertInternal(int number)
     {
-        if (number == 0m) builder.Insert(0, Map[0]);
+        if (_builder is null) throw new InvalidOperationException("Initialize StringBuilder first");
+
+        if (number == 0m) _builder.Insert(0, Map[0]);
 
         var iteration = 0;
-        foreach (var (low, high) in Step(number))
+        foreach (var (low, high) in GetTriplets(number))
         {
             if (low == 0 && high == 0)
             {
@@ -74,43 +85,45 @@ public static class NumberToWordsConverter
 
             if (iteration > 0)
             {
-                builder.Insert(0, Magnitude[iteration - 1]);
-                builder.Insert(0, ' ');
+                _builder.Insert(0, Magnitude[iteration - 1]);
+                _builder.Insert(0, ' ');
             }
 
-            if (low != 0) AppendWord(low, builder);
+            if (low != 0) AppendWord(low);
             if (high != 0)
             {
-                builder.Insert(0, Hundred);
-                builder.Insert(0, ' ');
-                AppendWord(high, builder);
+                _builder.Insert(0, Hundred);
+                _builder.Insert(0, ' ');
+                AppendWord(high);
             }
 
             iteration++;
         }
     }
 
-    private static void AppendWord(int number, StringBuilder builder)
+    private void AppendWord(int number)
     {
+        if (_builder is null) throw new InvalidOperationException("Initialize StringBuilder first");
+
         if (number < 20)
-            builder.Insert(0, Map[number]);
+            _builder.Insert(0, Map[number]);
         else
         {
             var low = number % 10;
             if (low != 0)
             {
-                builder.Insert(0, Map[low]);
-                builder.Insert(0, '-');
+                _builder.Insert(0, Map[low]);
+                _builder.Insert(0, '-');
             }
 
             var high = number - low;
-            builder.Insert(0, Map[high]);
+            _builder.Insert(0, Map[high]);
         }
 
-        builder.Insert(0, ' ');
+        _builder.Insert(0, ' ');
     }
 
-    private static IEnumerable<(int Low, int High)> Step(int number)
+    private static IEnumerable<(int Low, int High)> GetTriplets(int number)
     {
         var current = number;
 
