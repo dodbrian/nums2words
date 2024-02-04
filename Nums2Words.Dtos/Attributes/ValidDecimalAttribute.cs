d136eb1944889526434596e7958594e8a02b9bb6
Denis Zimin <roydronov@gmail.com>
2024-02-04 16:21:28 +0100

    validate max value on backend

--- Nums2Words.Dtos/Attributes/ValidDecimalAttribute.cs
+++ Nums2Words.Dtos/Attributes/ValidDecimalAttribute.cs
@@ -5,6 +5,8 @@ namespace Nums2Words.Dtos.Attributes;
 
 public class ValidDecimalAttribute : ValidationAttribute
 {
+    public double Max { get; init; }
+
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
         if (value is null) return new ValidationResult("Value cannot be null");
@@ -13,8 +15,11 @@ public class ValidDecimalAttribute : ValidationAttribute
         // Number format provided in the requirements matches fr-FR culture info.
         var specificCulture = CultureInfo.CreateSpecificCulture("fr-FR");
 
-        return decimal.TryParse(stringValue, specificCulture, out _)
-            ? ValidationResult.Success
-            : new ValidationResult("Provided value cannot be parsed as decimal");
+        if (!decimal.TryParse(stringValue, specificCulture, out var parsedValue))
+            return new ValidationResult("Provided value cannot be parsed as decimal");
+
+        return Math.Abs(parsedValue) > (decimal)Max
+            ? new ValidationResult($"Provided value cannot be bigger than {Max}")
+            : ValidationResult.Success;
     }
 }
--- Nums2Words.Dtos/MoneyConversionModel.cs
+++ Nums2Words.Dtos/MoneyConversionModel.cs
@@ -2,4 +2,4 @@ using Nums2Words.Dtos.Attributes;
 
 namespace Nums2Words.Dtos;
 
-public record MoneyConversionModel([ValidDecimal] string Amount);
+public record MoneyConversionModel([ValidDecimal(Max = 999_999_999.99)] string Amount);
